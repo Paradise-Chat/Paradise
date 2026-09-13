1c98d4fc1878e58e0c09338fbb28c4c9693216ec
GigiaJ <gigiaboone@yahoo.com>
2026-09-13 03:38:33 -0500

    tangled (merged core)

--- src/paradise/ui/container/call/events.cljs
+++ src/paradise/ui/container/call/events.cljs
@@ -1,201 +1,551 @@
 (ns paradise.ui.container.call.events
-  (:require [re-frame.core :as rf]
-            [taoensso.timbre :as log]
-            [cljs-workers.core :as main]
-            [clojure.string :as str]
-            [paradise.shared.client.state :as state]
-            [paradise.ui.container.call.call-container :refer [primary-iframe-ref backup-iframe-ref apply-iframe-sound-state!]]))
-
-(defn get-active-iframe-ref [db]
-  (let [active-iframe (get-in db [:call :active-iframe])
-        visible-iframe (get-in db [:call :visible-iframe] :primary)
-        target (or active-iframe visible-iframe)]
-    (if (= target :primary) primary-iframe-ref backup-iframe-ref)))
-
-(defn get-visible-iframe-ref [db]
-  (let [visible-iframe (get-in db [:call :visible-iframe] :primary)]
-    (if (= visible-iframe :primary) primary-iframe-ref backup-iframe-ref)))
-
-(defn click-iframe-button! [iframe-ref test-id]
-  (when-let [iframe @iframe-ref]
-    (let [doc (or (.-contentDocument iframe) (.. iframe -contentWindow -document))]
-      (if-let [btn (.querySelector doc (str "[data-testid='" test-id "']"))]
-        (.click btn)
-        (log/error "Could not find button with test-id:" test-id)))))
-
-(defn send-widget-action! [db action data]
-  (let [iframe-ref     (get-active-iframe-ref db)
-        active-room-id (get-in db [:call :active-room-id])]
-    (when-let [iframe @iframe-ref]
-      (let [widget-id  (str "element-call-" active-room-id)
-            req-id     (str action "-" (.now js/Date))
-            msg        #js {:api "toWidget", :action action, :widgetId widget-id, :requestId req-id, :data (clj->js data)}]
-        (try (.postMessage (.-contentWindow iframe) msg "*")
-             (catch :default e (log/error "Failed to send action" action "to iframe:" e)))))))
+  (:require
+   [re-frame.core :as rf]
+   [cljs-workers.core :as main]
+   [cljs-workers.mesh :as mesh]
+   [cljs.core.async :refer [go <!]]
+   [paradise.shared.client.state :as state]
+   [promesa.core :as p]
+   [taoensso.timbre :as log]
+   [paradise.ui.container.call.runtime :as rtc]
+   [paradise.ui.container.call.native :as native]))
 
 (rf/reg-event-fx
- :call/recv-widget-message
- (fn [{:keys [db]} [_ msg-string]]
-   (let [parsed         (js/JSON.parse msg-string)
-         widget-id      (.-widgetId parsed)
-         target-room-id (when widget-id (str/replace widget-id #"^element-call-" ""))
-         iframes        (get-in db [:call :iframes])
-         iframe-ref     (cond
-                          (= (:primary iframes) target-room-id) primary-iframe-ref
-                          (= (:backup iframes) target-room-id)  backup-iframe-ref
-                          :else (get-visible-iframe-ref db))]
-     (when-let [iframe @iframe-ref]
-       (try (.postMessage (.-contentWindow iframe) parsed "*")
-            (catch :default e (log/error "Failed to pipe worker message to iframe:" e)))))
-   {}))
+ :call/initiate-e2ee
+ (fn [{:keys [db]} [_ room-id]]
+     (mesh/do-with-thread! :engine-pool
+                         {:handler :call/broadcast-e2ee-key
+                          :arguments {:room-id room-id}})
+
+
+   {:db db}))
+
+
+(rf/reg-event-fx
+ :call/get-key-provider
+ (fn [_]
+   (go
+       (let [res (<! (mesh/do-with-thread!
+                      :engine-pool
+                      {:handler :call/get-key-provider
+                       :arguments {}}))]
+         (when (and res (:provider res))
+           (js/console.log "Engine signaled KeyProvider ready"))))))
+
+
+
+
+(rf/reg-event-fx
+ :call/e2ee-key-received
+ (fn [{:keys [db]} [_ {:keys [participant-id key-index key-array room-id]}]]
+
+   (rtc/inject-key! participant-id key-index key-array)
+
+   {:db (assoc-in db [:call :e2ee-keys participant-id key-index] key-array)}))
+
+
+
+(rf/reg-event-fx
+ :call/init-widget
+ (fn [{:keys [db]} [_ room-id]]
+   (js/console.log "DISPATCHED INIT WIDGET")
+   (mesh/do-with-thread! :engine-pool {:handler :call/start-crypto-driver :arguments {:room-id room-id}})
+   {:db db}))
+
+
+
+(rf/reg-event-fx
+ :call/start
+ (fn [{:keys [db]} [_ room-id]]
+   (when-let [pool @state/!engine-pool]
+     (go
+       (let [res (<! (main/do-with-pool! pool
+                                         {:handler :call/request-sfu-token
+                                          :arguments {:room-id room-id}}))]
+         (if (or (= (:status res) :success) (= (:status res) "success"))
+           (do
+             (js/console.log "LiveKit Payload Received:" res)
+             (rf/dispatch [:call/sfu-token-received {:url (:url res) :token (:token res)}]))
+           (log/error "Engine failed to negotiate LiveKit token:" (:msg res))))))
+
+   {:db (-> db
+            (assoc-in [:call :active-room-id] room-id)
+            (assoc-in [:call :is-active?] true)
+            (assoc-in [:call :status] :requesting-token))}))
+
+(rf/reg-event-fx
+ :call/sfu-token-received
+ (fn [{:keys [db]} [_ {:keys [url token]}]]
+   {:db (-> db
+            (assoc-in [:call :livekit-url] url)
+            (assoc-in [:call :livekit-token] token)
+            (assoc-in [:call :status] :connected))}))
+
+(rf/reg-event-fx
+ :call/hangup
+ (fn [{:keys [db]} _]
+   (when-let [pool @state/!engine-pool]
+     (main/do-with-pool! pool {:handler :call/leave-sfu}))
+   {:db (-> db
+            (assoc-in [:call :active-room-id] nil)
+            (assoc-in [:call :is-active?] false)
+            (assoc-in [:call :livekit-url] nil)
+            (assoc-in [:call :livekit-token] nil)
+            (assoc-in [:call :e2ee-keys] nil)
+            (assoc-in [:call :status] :idle))}))
+
+(rf/reg-fx
+ :livekit/set-microphone
+ (fn [enabled?]
+   (-> (p/let [_ (rtc/set-microphone-enabled! enabled?)]
+         (rf/dispatch
+          [:call/media-state-changed
+           :audio
+           enabled?]))
+
+       (p/catch
+        (fn [err]
+          (log/error
+           err
+           "Failed to change microphone state")
+
+          (rf/dispatch
+           [:call/media-error
+            :microphone
+            err]))))))
+
+
+(rf/reg-fx
+ :livekit/set-camera
+ (fn [enabled?]
+   (-> (p/let [_ (rtc/set-camera-enabled! enabled?)]
+         (rf/dispatch
+          [:call/media-state-changed
+           :video
+           enabled?]))
+
+       (p/catch
+        (fn [err]
+          (log/error
+           err
+           "Failed to change camera state")
+
+          (rf/dispatch
+           [:call/media-error
+            :camera
+            err]))))))
+
+
+(rf/reg-fx
+ :livekit/set-screen-share
+ (fn [enabled?]
+   (-> (p/let [_ (rtc/set-screen-share-enabled! enabled?)]
+         (rf/dispatch
+          [:call/media-state-changed
+           :screen-sharing
+           enabled?]))
+
+       (p/catch
+        (fn [err]
+          (log/error
+           err
+           "Failed to change screen share state")
+
+          (rf/dispatch
+           [:call/media-error
+            :screen-share
+            err]))))))
+
+
+(rf/reg-fx
+ :livekit/inject-e2ee-key
+ (fn [{:keys [participant-id
+              key-index
+              key-array]}]
+
+   (-> (p/let [_ (rtc/inject-key!
+                  participant-id
+                  key-index
+                  key-array)]
+         nil)
+
+       (p/catch
+        (fn [err]
+          (log/error
+           err
+           "Failed to inject LiveKit E2EE key"
+           participant-id
+           key-index)
+
+          (rf/dispatch
+           [:call/e2ee-error
+            {:participant-id participant-id
+             :key-index key-index
+             :error err}]))))))
+
+
+(rf/reg-fx
+ :livekit/disconnect
+ (fn [_]
+   (-> (p/let [_ (rtc/disconnect!)]
+         nil)
+
+       (p/catch
+        (fn [err]
+          (log/warn
+           err
+           "LiveKit disconnect returned an error"))))))
+
+
+
+
+(rf/reg-event-fx
+ :call/e2ee-key-received
+ (fn [_ [_ {:keys [participant-id
+                   key-index
+                   key-array]}]]
+
+   {:livekit/inject-e2ee-key
+    {:participant-id participant-id
+     :key-index key-index
+     :key-array key-array}}))
+
+
+(rf/reg-event-db
+ :call/e2ee-error
+ (fn [db [_ error]]
+   (assoc-in
+    db
+    [:call :e2ee-error]
+    error)))
+
+
+
+(rf/reg-event-fx
+ :call/toggle-audio
+ (fn [{:keys [db]} _]
+   {:livekit/set-microphone
+    (not
+     (get-in
+      db
+      [:call :audio-enabled?]
+      true))}))
+
+
+(rf/reg-event-fx
+ :call/toggle-video
+ (fn [{:keys [db]} _]
+   {:livekit/set-camera
+    (not
+     (get-in
+      db
+      [:call :video-enabled?]
+      false))}))
+
+
+(rf/reg-event-fx
+ :call/toggle-screen-share
+ (fn [{:keys [db]} _]
+   {:livekit/set-screen-share
+    (not
+     (get-in
+      db
+      [:call :screen-sharing?]
+      false))}))
+
+
+(rf/reg-event-db
+ :call/toggle-deafen
+ (fn [db _]
+   (update-in
+    db
+    [:call :deafened?]
+    not)))
+
+
+
+(rf/reg-event-db
+ :call/media-state-changed
+ (fn [db [_ kind enabled?]]
+   (case kind
+
+     :audio
+     (assoc-in
+      db
+      [:call :audio-enabled?]
+      enabled?)
+
+     :video
+     (assoc-in
+      db
+      [:call :video-enabled?]
+      enabled?)
+
+     :screen-sharing
+     (assoc-in
+      db
+      [:call :screen-sharing?]
+      enabled?)
+
+     db)))
+
+
+(rf/reg-event-db
+ :call/update-media-state
+ (fn [db [_ {:keys [audio
+                    video
+                    screen-sharing]}]]
+
+   (cond-> db
+
+     (some? audio)
+     (assoc-in
+      [:call :audio-enabled?]
+      audio)
+
+     (some? video)
+     (assoc-in
+      [:call :video-enabled?]
+      video)
+
+     (some? screen-sharing)
+     (assoc-in
+      [:call :screen-sharing?]
+      screen-sharing))))
+
+
+(rf/reg-event-db
+ :call/media-error
+ (fn [db [_ device err]]
+   (assoc-in
+    db
+    [:call :media-error]
+    {:device device
+     :error err})))
+
+(rf/reg-event-db
+ :call/set-active-room
+ (fn [db [_ room-id]]
+   (log/info
+    "Call: Setting active room to"
+    room-id)
+
+   (-> db
+       (assoc-in
+        [:call :active-room-id]
+        room-id)
+
+       (assoc-in
+        [:call :audio-enabled?]
+        true)
+
+       (assoc-in
+        [:call :video-enabled?]
+        false)
+
+       (assoc-in
+        [:call :screen-sharing?]
+        false)
+
+       (assoc-in
+        [:call :disconnecting?]
+        false)
+
+       (assoc-in
+        [:call :media-error]
+        nil)
+
+       (assoc-in
+        [:call :e2ee-error]
+        nil))))
+
+
+(rf/reg-event-db
+ :call/set-active
+ (fn [db [_ active?]]
+   (assoc-in
+    db
+    [:call :is-active?]
+    active?)))
 
 
 (rf/reg-event-fx
  :call/hangup
  (fn [{:keys [db]} [_ opts]]
-   (let [wipe?          (get opts :wipe-state? true)
-         skip-native?   (get opts :skip-native? false)
-         target-room-id (or (:room-id opts) (get-in db [:call :active-room-id]))
-         iframes        (get-in db [:call :iframes])
-         iframe-key     (cond
-                          (= (:primary iframes) target-room-id) :primary
-                          (= (:backup iframes) target-room-id) :backup
-                          :else nil)
-         iframe-ref     (cond
-                          (= iframe-key :primary) primary-iframe-ref
-                          (= iframe-key :backup)  backup-iframe-ref
-                          :else nil)]
-     (when (and target-room-id (not skip-native?))
-       (paradise.ui.container.call.native/end-call! target-room-id))
-     (when iframe-ref
-       (click-iframe-button! iframe-ref "incall_leave")
-       (when-let [iframe @iframe-ref]
-         (let [widget-id (str "element-call-" target-room-id)
-               req-id    (str "io.element.hangup-" (.now js/Date))
-               msg       #js {:api "toWidget" :action "io.element.hangup" :widgetId widget-id :requestId req-id :data #js {}}]
-           (try (.postMessage (.-contentWindow iframe) msg "*") (catch :default _)))))
-     (if wipe?
-       {:db (-> db
-                (assoc-in [:call :active-room-id] nil)
-                (assoc-in [:call :active-iframe] nil)
-                (assoc-in [:call :iframes] {:primary nil :backup nil})
-                (assoc-in [:call :is-active?] false))
-        :dispatch [:call/teardown]}
-       (let [new-db (cond-> db
-                      iframe-key (assoc-in [:call :iframes iframe-key] nil)
-                      true       (assoc-in [:call :active-room-id] nil))]
-         {:db new-db})))))
+   (let [opts
+         (or opts {})
 
-(rf/reg-event-fx
- :call/toggle-audio
- (fn [{:keys [db]} _]
-   (let [audio-enabled? (get-in db [:call :audio-enabled?] true)
-         video-enabled? (get-in db [:call :video-enabled?] false)
-         new-state      (not audio-enabled?)]
-     (send-widget-action! db "io.element.device_mute"
-                          {:audio_enabled new-state
-                           :video_enabled video-enabled?})
-     {:db (assoc-in db [:call :audio-enabled?] new-state)})))
+         room-id
+         (or (:room-id opts)
+             (get-in
+              db
+              [:call :active-room-id]))
 
-(rf/reg-event-fx
- :call/toggle-video
- (fn [{:keys [db]} _]
-   (let [audio-enabled? (get-in db [:call :audio-enabled?] true)
-         video-enabled? (get-in db [:call :video-enabled?] false)
-         new-state      (not video-enabled?)]
-     (send-widget-action! db "io.element.device_mute"
-                          {:audio_enabled audio-enabled?
-                           :video_enabled new-state})
-     {:db (assoc-in db [:call :video-enabled?] new-state)})))
+         skip-native?
+         (get opts :skip-native? false)]
 
-(rf/reg-event-fx
- :call/toggle-deafen
- (fn [{:keys [db]} _]
-   (let [new-deafen-state (not (get-in db [:call :deafened?] false))]
-     (apply-iframe-sound-state! primary-iframe-ref (not new-deafen-state))
-     (apply-iframe-sound-state! backup-iframe-ref (not new-deafen-state))
-     {:db (assoc-in db [:call :deafened?] new-deafen-state)})))
+     (when (and room-id
+                (not skip-native?))
+       (native/end-call! room-id))
+
+     {:db
+      (assoc-in
+       db
+       [:call :disconnecting?]
+       true)
+
+      :livekit/disconnect
+      true})))
 
 
 (rf/reg-event-fx
- :call/toggle-screen-share
- (fn [{:keys [db]} _] (click-iframe-button! (get-active-iframe-ref db) "incall_screenshare") {}))
+ :call/livekit-disconnected
+ (fn [{:keys [db]} [_ reason]]
+   (let [room-id
+         (get-in
+          db
+          [:call :active-room-id])
 
-(rf/reg-event-fx
- :call/open-settings
- (fn [{:keys [db]} _]
-   (when-let [iframe @(get-visible-iframe-ref db)]
-     (let [doc (or (.-contentDocument iframe) (.. iframe -contentWindow -document))
-           btn (.querySelector doc "button[aria-labelledby=':rt:']")]
-       (if btn (.click btn) (log/error "Settings button not found")))) {}))
+         disconnecting?
+         (get-in
+          db
+          [:call :disconnecting?]
+          false)]
 
+     (log/info
+      "LiveKit disconnected:"
+      reason)
 
-(rf/reg-event-db
- :call/toggle-chat
- (fn [db _]
-   (update-in db [:call :chat-open?] not)))
+     (when (and room-id
+                (not disconnecting?))
+       (native/end-call! room-id))
 
-(rf/reg-event-db
- :call/toggle-iframe
- (fn [db _]
-   (let [current-vis (get-in db [:call :visible-iframe] :primary)]
-     (assoc-in db [:call :visible-iframe] (if (= current-vis :primary) :backup :primary)))))
+     {:dispatch
+      [:call/teardown]})))
 
-(rf/reg-event-fx
- :call/handle-widget-hangup
- (fn [{:keys [db]} [_ widget-id]]
-   (let [target-room-id (when widget-id (str/replace widget-id #"^element-call-" ""))
-         active-room-id (get-in db [:call :active-room-id])]
-     (when target-room-id (paradise.ui.container.call.native/end-call! target-room-id))
-
-     (if (and active-room-id (= target-room-id active-room-id))
-       {:dispatch [:call/teardown]}
-       {}))))
 
 (rf/reg-event-fx
  :call/teardown
  (fn [{:keys [db]} _]
-   (when-let [pool @state/!engine-pool]
-     (main/do-with-pool! pool {:handler :teardown-widget}))
-   (when-let [p-iframe @primary-iframe-ref]
-     (js/setTimeout #(set! (.-src p-iframe) "about:blank") 5000))
-   (when-let [b-iframe @backup-iframe-ref]
-     (js/setTimeout #(set! (.-src b-iframe) "about:blank") 5000))
+   (rtc/reset-session!)
 
-   {:db (-> db
-            (assoc-in [:call :active-room-id] nil)
-            (assoc-in [:call :active-iframe] nil)
-            (assoc-in [:call :iframes] {:primary nil :backup nil})
-            (assoc-in [:call :is-active?] false)
-            (assoc :main-focus :timeline))}))
+   {:db
+    (-> db
+        (assoc-in
+         [:call :active-room-id]
+         nil)
+
+        (assoc-in
+         [:call :is-active?]
+         false)
+
+        (assoc-in
+         [:call :disconnecting?]
+         false)
+
+        (assoc-in
+         [:call :screen-sharing?]
+         false)
+
+        (assoc-in
+         [:call :media-error]
+         nil)
+
+        (assoc-in
+         [:call :e2ee-error]
+         nil)
+
+        (assoc
+         :main-focus
+         :timeline))}))
+
+(rf/reg-event-db
+ :call/toggle-chat
+ (fn [db _]
+   (update-in
+    db
+    [:call :chat-open?]
+    not)))
+
+(rf/reg-sub
+ :call/state
+ (fn [db _]
+   (:call db)))
 
 
-(rf/reg-event-db :call/set-active-room
- (fn [db [_ room-id]]
-   (log/info "Call: Setting active room to" room-id)
-   (-> db
-       (assoc-in [:call :active-room-id] room-id)
-       (assoc-in [:call :audio-enabled?] true)
-       (assoc-in [:call :video-enabled?] false))))
+(rf/reg-sub
+ :call/active-room
+ (fn [db _]
+   (get-in
+    db
+    [:call :active-room-id])))
 
 
-(rf/reg-event-db :call/set-active
- (fn [db [_ active?]]
-   (assoc-in db [:call :is-active?] active?)))
+(rf/reg-sub
+ :call/is-active?
+ (fn [db _]
+   (get-in
+    db
+    [:call :is-active?]
+    false)))
 
 
-(rf/reg-event-db :call/update-media-state
- (fn [db [_ {:keys [audio video]}]]
-   (-> db
-       (assoc-in [:call :audio-enabled?] audio)
-       (assoc-in [:call :video-enabled?] video))))
+(rf/reg-sub
+ :call/audio-enabled?
+ (fn [db _]
+   (get-in
+    db
+    [:call :audio-enabled?]
+    true)))
 
-(rf/reg-sub :call/deafened?        (fn [db _] (get-in db [:call :deafened?] false)))
-(rf/reg-sub :call/screen-sharing?  (fn [db _] (get-in db [:call :screen-sharing?] false)))
-(rf/reg-sub :call/audio-enabled?   (fn [db _] (get-in db [:call :audio-enabled?] true)))
-(rf/reg-sub :call/video-enabled?   (fn [db _] (get-in db [:call :video-enabled?] false)))
-(rf/reg-sub :call/chat-open?       (fn [db _] (get-in db [:call :chat-open?] false)))
-(rf/reg-sub :call/state            (fn [db _] (:call db)))
-(rf/reg-sub :call/active-room      (fn [db _] (get-in db [:call :active-room-id])))
-(rf/reg-sub :call/is-active?       (fn [db _] (get-in db [:call :is-active?])))
-(rf/reg-sub :call/is-primary-iframe? (fn [db _] (= (get-in db [:call :visible-iframe] :primary) :primary)))
+
+(rf/reg-sub
+ :call/video-enabled?
+ (fn [db _]
+   (get-in
+    db
+    [:call :video-enabled?]
+    false)))
+
+
+(rf/reg-sub
+ :call/deafened?
+ (fn [db _]
+   (get-in
+    db
+    [:call :deafened?]
+    false)))
+
+
+(rf/reg-sub
+ :call/screen-sharing?
+ (fn [db _]
+   (get-in
+    db
+    [:call :screen-sharing?]
+    false)))
+
+
+(rf/reg-sub
+ :call/chat-open?
+ (fn [db _]
+   (get-in
+    db
+    [:call :chat-open?]
+    false)))
+
+
+(rf/reg-sub
+ :call/media-error
+ (fn [db _]
+   (get-in
+    db
+    [:call :media-error])))
+
+
+(rf/reg-sub
+ :call/e2ee-error
+ (fn [db _]
+   (get-in
+    db
+    [:call :e2ee-error])))
