2918835bf896b663f54aeba03afbb86950173682
GigiaJ <gigiaboone@yahoo.com>
2026-09-13 03:38:57 -0500

    tangled

--- src/paradise/binding/streams.cljs
+++ src/paradise/binding/streams.cljs
@@ -3,6 +3,18 @@
             [paradise.binding.nostr :as nostr]
             ))
 
+;; Should move this to core and place a method inside core
+;; to enable the engine to call the main thread and
+;; adjust the registration of components.
+;; Removal is easy as it is through the registry
+;; Addition would need to be treated as a plugin
+;; Most runtime plugins won't carry heavy weight
+;; as the timeline handling is offloaded from the
+;; main thread
+;; As such it can further be broken up to scale
+;; cleaner as well
+
+
 (defn init-engine-streams! [engine-id]
   (case (keyword engine-id)
     :matrix (matrix/init!)
