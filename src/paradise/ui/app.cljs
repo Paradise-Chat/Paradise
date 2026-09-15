45092a306a4333527058b1b2f72dc907845ca926
GigiaJ <gigiaboone@yahoo.com>
2026-09-15 14:49:14 -0500

    tangled

--- src/paradise/ui/app.cljs
+++ src/paradise/ui/app.cljs
@@ -331,7 +331,6 @@
 (defn ^:export init []
   (re-frame/dispatch-sync [:initialize-db])
   (init-native-listeners!)
-  (re-frame/dispatch [:app/thread-boot])
   (re-frame/dispatch [:app/load-settings-by-stage :boot])
   (re-frame/dispatch [:app/start-boot-sequence])
   (mount-root))
